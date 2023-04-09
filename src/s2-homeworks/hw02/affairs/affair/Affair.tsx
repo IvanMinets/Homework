0d540a76358c01336da6a3e464ce85a5175a5eab
IvanMinets <minetssmash@gmail.com>
2023-04-09 18:52:09 +0300

    s

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -10,7 +10,7 @@ type AffairPropsType = {
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = (props.affair._id) => {
+    const deleteCallback = () => {
 
         // need to fix
     }
