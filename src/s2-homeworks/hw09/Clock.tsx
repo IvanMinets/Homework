bd7e16e1e22811393181890d24b9faa402518dae
Ivan Minets2 <minetssmash@gmail.com>
2023-05-25 16:38:51 +0300

    Update Clock.tsx

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -14,11 +14,11 @@ function Clock() {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
         if (!isRunning) {
-            const newIntervalID = window.setInterval(() => {
+            const newTimerId = window.setInterval(() => {
                 setDate(new Date());
             }, 1000);
             setIsRunning(true);
-            setTimerId(newIntervalID);
+            setTimerId(newTimerId);
         }
     }
 
@@ -39,7 +39,7 @@ function Clock() {
     }
 
     const stringTime =`${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString("en-US", ) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = date.toLocaleDateString('ru-Ru') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleDateString("en-US", {weekday: "long"} ) || <br/> // пишут студенты
